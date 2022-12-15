8e41145dcd3ca1a66889752c8047b052525c15f6
Zstzbxn <zstzbxn@pop-os.localdomain>
2022-12-15 17:32:37 +0100

    final push

--- solver.py
+++ solver.py
@@ -536,7 +536,7 @@ def main():
 
     timer = {}
     soluce = {}
-    for i in range(10):
+    for i in range(2000):
         cube.reset()
         scramble = cube.reducePattern(cube.random(randint(20, 200)))
         cube.scramble(scramble)
@@ -561,39 +561,3 @@ if __name__ == "__main__":
 
 
 
-# https://www.speedsolving.com/wiki/index.php/Thistlethwaite%27s_algorithm
-# https://math.stackexchange.com/questions/1362471/rubiks-cube-thistlethwaite-four-phase-algorithm
-
-
-# Phase 1 <U,D,L,R,F2,B2> F and B to switch BAD to GOOD and GOOD to BAD
-# EO (edge orientation) : all edge to good pos, (ZZ method) 7 moves worst case
-# https://www.speedsolving.com/wiki/index.php/Edge_Orientation#ZZ
-
-
-
-# Phase 2 <U,D,L,R,F2,B2> 
-# Corner orientation :  10 moves worst case
-# 2.1 placement of U/D edges in U/D faces (whatever if it's U or D just place it on U or D)
-# https://www.speedsolving.com/wiki/index.php/Edge_Orientation#2-axis_EO
-
-# 2.2 Corner Orientation
-# https://www.cuberoot.me/dr-trigger/
-# search for a known DR trigger (can be a 2c or 3c trigger if no 4c found for 4+ count) (U D F2 B2 R2 L2)
-# search on mirror axies too (addapt algo to mirrored)
-
-# Phase 3 <U,D,L2,R2,F2,B2>
-# Every colors are on there face or the opposit, 13 moves worst case
-# https://www.ryanheise.com/cube/human_thistlethwaite_algorithm.html
-# 3.1 Corners
-# 3.2 Edges
-
-
-# Phase 4 <U2,D2,L2,R2,F2,B2> 
-# Final resolution : 15 moves worst case
-# block building
-
-
-
-
-
-# F2 D2 R' F2 R D2 F2 R2 U' F D R2 B2 L' F' R D' U2 F R2
